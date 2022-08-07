2289bf9f0c838d5bfcde65a42c3d5d329a9ac60a
Gonzalo <mgmediaweb@gmail.com>
2022-08-06 19:03:42 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useQuery, gql } from "@apollo/client";
 import './App.css';
 
+const FILMS_QUERY = gql`
+  {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`;
+
 function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.launchesPast.map((launch) => (
+          <li key={launch.id}>{launch.mission_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+//const GITHUB_BASE_URL = 'https://docs.github.com/en/free-pro-team@latest/graphql/reference/objects#topic';
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  // uri: "https://docs.github.com/en/free-pro-team@latest/graphql/reference/objects#topic",
+  cache: new InMemoryCache()
+});
+
+const github_data = {
+  "token": "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",
+  "username": "mgmediaweb",
+};
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authentication: `bearer ${github_data.token}`
+};
+
+const body = {
+  "query": `
+    query {
+      __type(name:"Repository") {
+        fields {
+          name
+          description
+          type {
+            kind
+            name
+            description
+          }
+          args {
+            name
+            description
+            type {
+              kind
+              name
+              description
+            }
+            defaultValue
+          }
+        }
+      }
+    }`
+};
+
+fetch(GITHUB_BASE_URL, {
+  method: 'POST',
+  headers: headers,
+  mode: 'no-cors',
+  body: JSON.stringify(body),
+})
+.then(response => console.log('then => ', JSON.stringify(response)))
+.catch(err => console.log('err => ', err));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
